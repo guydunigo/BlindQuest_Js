f2c8b663a5ea4c28243e3a75b39942b0cc10c0a6
GONI Guillaume <gdn3@live.fr>
2017-08-30 11:36:16 +0200

    Init project with BrocFile, other confs, and base html/js files. + Locked broccoli's dep broccoli-slow-trees to 2.0.0 (serve bug)

---
+++ Brocfile.js
@@ -0,0 +1,77 @@
+// Broccoli utils
+const funnel = require("broccoli-funnel");
+const merge = require("broccoli-merge-trees");
+//const concat = require("broccoli-concat");
+// Js
+const Rollup = require("broccoli-rollup");
+const babel = require("broccoli-babel-transpiler");
+// Dev utils
+const LiveReload = require("broccoli-livereload");
+const env = require("broccoli-env").getEnv();
+// Options
+const root = "src";
+const createSourceMap = (env === "development") ? true : false;
+
+/* --------------------------- html --------------------------- */
+const html = funnel(root, {
+    files: ["index.html"],
+    destDir: "/"
+});
+
+/* --------------------------- css --------------------------- */
+// const css = funnel(root, {
+//         files: ["stylesheet.css"],
+//         destDir: "/"
+// })
+
+/* --------------------------- js --------------------------- */
+let js = funnel(root, {
+    include: ["**/*.js"]
+});
+js = new Rollup(js, {
+    inputFiles: ["**/*.js"],
+    rollup: {
+        entry: "js/main.js",
+        dest: "script.js",
+        sourceMap: createSourceMap,
+        format: "es"
+    }
+});
+js = babel(js, {
+    // browserPolyfill: true, // doesn"t work
+    sourceMap: createSourceMap,
+    compact: "true",
+    presets: [["env", {
+        target: {
+            browsers: "last 2 versions"
+        }
+    }]]
+});
+
+// /* -------------- Grab the polyfill for babel -------------- */
+// const babelPolyfillPath = "node_modules/babel-polyfill/dist";
+// const babelPolyfillFileName = "polyfill.min.js";
+// const babelPolyfill = funnel(babelPolyfillPath, {
+//     files: [babelPolyfillFileName],
+//     destDir: "babel-polyfill"
+// });
+// /* --------------------------------------------------------- */
+// js = merge([babelPolyfill, js]);
+// js = concat(js, {
+//     headerFiles: ["babel-polyfill/" + babelPolyfillFileName],
+//     inputFiles: ["**/*.js"],
+//     outputFile: "script.js",
+//     sourceMapConfig: { enabled: createSourceMap }
+// })
+// /* --------------------------------------------------------- */
+
+/* --------------------------- end --------------------------- */
+let tree = merge([html,/*css,*/js]);
+
+if (env === "development") {
+    tree = new LiveReload(tree, {
+        target: "index.html"
+    });
+}
+
+module.exports = tree;
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+const main = function() {
+    document.getElementById("msg")
+        .textContent = "Game loaded, have fun!"
+}
+
+document.addEventListener("DOMContentLoaded", main);
